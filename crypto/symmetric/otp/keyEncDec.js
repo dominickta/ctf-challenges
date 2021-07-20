22de0eb7a68a2c7f062993b71a4ff869e8031ee5
Dominick Ta <domta@cs.washington.edu>
2021-07-19 21:55:47 -0700

    Implement 'decrypt()' (not tested yet)

--- crypto/symmetric/otp/keyEncDec.js
+++ crypto/symmetric/otp/keyEncDec.js
@@ -1,1 +1,14 @@
-
+function decrypt(cipher, key) {
+	if (cipher.length != key.length) {
+		return null;
+	}
+	let decryptedBytes = [];
+	for (let i = 0; i < cipher.length; i++) {
+		decryptedBytes.push(cipher[i] ^ key[i]);
+	}
+	let msg = ""
+	for (let i = 0; i < decryptedBytes.length; i++) {
+		msg += String.fromCharCode(decryptedBytes[i]);
+	}
+	return msg;
+}
