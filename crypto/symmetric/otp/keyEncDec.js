61a414ba045a31d07e78d1882d11650af89d6948
Chanly Ly - I <c.ly@f5.com>
2021-07-20 13:20:55 -0700

    added comments to code

--- crypto/symmetric/otp/keyEncDec.js
+++ crypto/symmetric/otp/keyEncDec.js
@@ -1,11 +1,21 @@
+/**
+ * Decrypt a message given the key that was used to encrypt
+ * 
+ * @param cipher a encrypted message that you want to decrypt
+ * @param key a byte array that you will use to dencrypt the message
+ * @returns a decrypted message
+ */
 function decrypt(cipher, key) {
 	if (cipher.length != key.length) {
 		return null;
 	}
+
+	//XOR the byte arrays to decrypt the message
 	let decryptedBytes = [];
 	for (let i = 0; i < cipher.length; i++) {
 		decryptedBytes.push(cipher[i] ^ key[i]);
 	}
+	//convert byte array back to string
 	let msg = ""
 	for (let i = 0; i < decryptedBytes.length; i++) {
 		msg += String.fromCharCode(decryptedBytes[i]);
@@ -13,8 +23,17 @@ function decrypt(cipher, key) {
 	return msg;
 }
 
-
-function encryption(msg, key){
+/**
+ * Encrypt a message given a certain key
+ * 
+ * @param msg a string that you want to encrypt
+ * @param key a byte array that you will use to encrypt the message
+ * @returns a encrypted message in byte array
+ */
+function encryption(msg, key = null){
+	if (msg.length != key.length) {
+		key = keyGen(msg.length);
+	}
     let encoder = new TextEncoder();
     let msgbyte = encoder.encode(msg);
     var cipher = [];
