545309374cc8c0ec9e75d9c2780dc00db37a01e8
Chanly Ly - I <c.ly@f5.com>
2021-07-23 14:48:54 -0700

    modified block cipher to use table

--- crypto/symmetric/block cipher/encDec.js
+++ crypto/symmetric/block cipher/encDec.js
@@ -1,18 +1,68 @@
+const table = [
+    [0, 12, 2, 6, 12, 15, 15, 2, 12],
+    [1, 6, 0, 15, 0, 11, 10, 7, 5],
+    [2, 10, 14, 2, 14, 6, 8, 14, 11], 
+    [3, 15, 1, 7, 4, 3, 5, 4, 10],
+    [4, 14, 5, 12, 8, 1, 7, 11, 3],
+    [5, 0, 13, 11, 11, 8, 14, 0, 7],
+    [6, 11, 10, 0, 2, 14, 6, 3, 1],
+    [7, 1, 9, 9, 7, 9, 3, 10, 9],
+    [8, 4, 6, 4, 6, 5, 0, 6, 15],
+    [9, 7, 4, 4, 9, 2, 12, 9, 13],
+    [10, 5, 8, 8, 13, 7, 11, 8, 14],
+    [11, 8, 7, 13, 15, 12, 1, 13, 0],
+    [12, 9, 11, 10, 3, 13, 13, 5, 8],
+    [13, 13, 12, 5, 1, 0, 4, 12, 4],
+    [14, 2, 3, 1, 5, 4, 9, 15, 6],
+    [15, 3, 15, 3, 10, 10, 2, 1, 2]
+]
+const intToChar = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P']
+const mapCharToInt = new Map();
+mapCharToInt.set('A', 0);
+mapCharToInt.set('B', 1);
+mapCharToInt.set('C', 2);
+mapCharToInt.set('D', 3);
+mapCharToInt.set('E', 4);
+mapCharToInt.set('F', 5);
+mapCharToInt.set('G', 6);
+mapCharToInt.set('H', 7);
+mapCharToInt.set('I', 8);
+mapCharToInt.set('J', 9);
+mapCharToInt.set('K', 10);
+mapCharToInt.set('L', 11);
+mapCharToInt.set('M', 12);
+mapCharToInt.set('N', 13);
+mapCharToInt.set('O', 14);
+mapCharToInt.set('P', 15);
 
-function keyGen(length) {
-    key = Math.floor(Math.random() * (length - 1)) + 1;
+function keyGen() {
+    key = Math.floor(Math.random() * 6) + 1
     return key
 }
 
 function encryption (message, key) {
-    var encrypted = message.substring(key, key.length) +
-              message.substring(0, key);
-    return encrypted
-
+    //var arrInt = [];
+    var encrypted = "";
+    var keyindex = key + 1;
+    for(let i = 0; i < message.length; i++){
+        //arrInt[i] = map.get(message.charAt(i))
+        //arrInt[i] = table[arrInt[i]][key]
+        encrypted = encrypted + intToChar[table[mapCharToInt.get(message.charAt(i))][keyindex]]
+    }
+    return encrypted;
 }
 
+/*
 function decryption (message, key) {
+    keyindex = key + 1
+    var decrypted = "";
+    var keyindex = key + 1;
+    for(let i = 0; i < message.length; i++){
 
-    encryption(message, message.length - key)
+        //arrInt[i] = map.get(message.charAt(i))
+        //arrInt[i] = table[arrInt[i]][key]
+        decrypted = decrypted + intToChar[table[mapCharToInt.get(message.charAt(i))][keyindex]]
+    }
     return decrypted
 }
+*/
--- crypto/symmetric/ecp/encDec.js
+++ crypto/symmetric/ecp/encDec.js
@@ -1,13 +1,100 @@
 function keyGen(length) {
-    key = Math.floor(Math.random() * (length - 1)) + 1;
+    key = Math.floor(Math.random() * (length / 2)) + 2
     return key
 }
+function blocksizeGen(length) {
+    block = Math.floor(Math.random() * (length / 2)) + 2
+    return block
+}
+function encryption(message, key, blocksize) {
+    var encrypted = ""
+    for(let i = 0; i < Math.ceil(message.length/blocksize); i++) {
+        if(message.length >= blocksize) {
+            encrypted = encrypted + encryptionBC(message.substring(0, blocksize) ,key)
+            message = message.substring(blocksize, message.length + 1)
+        } else {
+            encrypted = encrypted + encryptionBC(message.substring(0, message.length + 1) ,key)
 
-function encryption (message, key) {
+        }
+
+    }
+    return encrypted
+
+}
+
+function encryptionBC (message, key) {
+    
     var encrypted = message.substring(key, key.length) +
               message.substring(0, key);
     return encrypted
+    
+    /*
+    if (key == 1)
+    {
+        console.log(message);
+        return message;
+    }
+    let str1 = message.split("");
 
+    // Find length of string
+    let len = message.length;
+
+    // Create an array of
+    // strings for all n rows
+    let arr = new Array(key);
+    for(let i=0;i<key;i++)
+    {
+        arr[i]="";
+    }
+     
+
+    // Initialize index for
+    // array of strings arr[]
+    let row = 0;
+    let down = true; // True if we are moving
+    // down in rows, else false
+
+    // Traverse through
+    // given string
+    for (let i = 0; i < len; ++i)
+    {
+        // append current character
+        // to current row
+        arr[row] += (str1[i]);
+
+        // If last row is reached,
+        // change direction to 'up'
+        if (row == key - 1)
+        {
+            down = false;
+        }
+          
+        // If 1st row is reached,
+        // change direction to 'down'
+        else if (row == 0)
+        {
+            down = true;
+        }
+
+        // If direction is down,
+        // increment, else decrement
+        if (down)
+        {
+            row++;
+        }
+        else
+        {
+            row--;
+        }
+    }
+    var encrypted = "";
+    for (let i = 0; i < key; ++i)
+    {
+        encrypted = encrypted + arr[i];
+    }
+    return encrypted
+
+    */
 }
 
 function decryption (message, key) {
